759462906bf9b9d684277af3c9e33002a4ec99d9
Judith Lippold <judith@MBP-von-Judith.fritz.box>
2023-02-01 10:56:46 +0100

    head title changed

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Github Actions App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
